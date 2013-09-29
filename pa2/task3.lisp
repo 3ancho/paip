e13126ae9fabd979819e4de911354a493bdb21c3
changkyu <changkyu@changkyu-ubuntu.(none)>
2013-09-29 17:39:56 -0400

    added task3

---
+++ pa2/task3.lisp
@@ -0,0 +1,9 @@
+(load "student.lisp")
+
+(setf example3-1 '(what is the sum of 2 times 3 and 4 ?))
+
+(setf example3-2 '(what is the product of 2 plus 3 and 4 ?))
+
+(setf example3-3 '(the square footage is twice the perimeter |.| the perimeter is 20 |.| what is the square footage ?))
+
+
