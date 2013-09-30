2c31d5674296a92e75a235e5584faaea0f4aed3f
Ruoran Wang <ruoranwang@gmail.com>
2013-09-30 13:31:18 -0400

    added examples for task2

--- pa2/task2.lisp
+++ pa2/task2.lisp
@@ -1,11 +1,8 @@
 
 (load "student.lisp")
 
-(setf example1 '(if the number of customers Tom gets is twice the square of 20 % of the number of ads he runs |,| and the number of ads is 45 |,| then what is customers ? ))
-
 (setf equations '((= 100 (+ x (* y 40)))
 		  (= x (* 10 y))))
-
 ; > (solve-equations equations)
 ; The equations to be solved are:
 ;    100 = (X + (Y * 40))
@@ -20,8 +17,32 @@
 		  (= x (- y 10))
 		  (= x z)))
 
+(setf three2 '((= x z)
+	       (= 100 (+ x (+ y z)))
+	       (= x (- y 10))))
+
+(setf example2 '(if tom's age plus lily's age is 4 |,| and tom's age minus lily's age is 0 |,| then what is tom's age ?))
+;CL-USER> (student example2)
+;
+;The equations to be solved are:
+;   (TOM + LILY) = 4
+;   (TOM - LILY) = 0
+;   WHAT = TOM
+;
+;The solution is:
+;   TOM = 2
+;   LILY = 2
+;   WHAT = 2
+;NIL
 
 (setf two-arg '((= (* 4 x) (- 100 x))))
-(solve equations nil)
+;CL-USER> (solve-equations two-arg)
+;
+;The equations to be solved are:
+;   (4 * X) = (100 - X)
+;
+;The solution is:
+;   X = 20
+;NIL
 
 
